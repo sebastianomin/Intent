206d061dbc93ec126687f8b552e50385b4619e6c
sebastianomingacci <mingacci.s@icloud.com>
2019-05-20 15:20:51 +0200

    switch

--- Intent/src/it/universityofpadova/biblioteca/BibliotecaTester.java
+++ Intent/src/it/universityofpadova/biblioteca/BibliotecaTester.java
@@ -17,33 +17,49 @@ public class BibliotecaTester {
 			System.out.println("4) stampa il contenuto della biblioteca");
 			System.out.println("5) esci");
 			int operazione = in.nextInt();
-			if (operazione == 1) {
+			switch (operazione) {
+			case 1:
 				System.out.println("Scegliere il nome utente:");
 				String a = in.nextLine();
 				String nome = in.nextLine();
 				System.out.println("Scegliere il cognome utente:");
 				String cognome = in.nextLine();
 				genna.aggiungiUtente(nome, cognome);
-			} else if (operazione == 2) {
+				
+				break;
+			case 2:
 				System.out.println("Scegliere il titolo del libro:");
-				String a = in.nextLine();
+				String b = in.nextLine();
 				String titolo = in.nextLine();
 				System.out.println("Scegliere il codice del libro:");
 				int codice = in.nextInt();
 				genna.aggiungiLibro(codice, titolo);
-			} else if (operazione == 3) {
+				
+				break;
+			case 3:
 				System.out.println("Scegliere il cognome dell'utente che richiede il prestito:");
-				String a = in.nextLine();
-				String cognome = in.nextLine();
+				String c = in.nextLine();
+				String cognomeP = in.nextLine();
 				System.out.println("Scegliere il codice del libro da prestare:");
-				int codice = in.nextInt();
-				genna.creaPrestito(codice, cognome);
-			} else if (operazione == 4) {
+				int codiceP = in.nextInt();
+				genna.creaPrestito(codiceP, cognomeP);
+				
+				break;
+			case 4:
 				System.out.println("Il contenuto della biblioteca:");
 				System.out.println(genna.toString());
-			} else if (operazione == 5) {
+				
+				break;
+			case 5:
+				System.out.println("******ARRIVEDERCI**************");
 				d = true;
+				
+				break;
+
+			default:
+				break;
 			}
+
 		}
 	}
 }
